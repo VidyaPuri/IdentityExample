71ed111d96fce632e57424373990125b9a041e46
Vid Črešnik <vid.cresnik@brinox.eu>
2020-10-06 08:19:04 +0200

    OAuth - EP.7

--- Basics/Controllers/HomeController.cs
+++ Basics/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using Basics.CustomPolicyProvider;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,6 +13,14 @@ namespace Basics.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IAuthorizationService _authorizationService;
+
+        public HomeController(IAuthorizationService authorizationService)
+        {
+            _authorizationService = authorizationService;
+        }
+
+
         public IActionResult Index()
         {
             return View();
@@ -35,6 +44,19 @@ namespace Basics.Controllers
             return View("Secret");
         }
 
+        [SecurityLevel(5)]
+        public IActionResult SecretLevel()
+        {
+            return View("Secret");
+        }
+
+        [SecurityLevel(15)]
+        public IActionResult SecretLevelHigher()
+        {
+            return View("Secret");
+        }
+
+        [AllowAnonymous]
         public IActionResult Authenticate()
         {
             var grandmaClaims = new List<Claim>()
@@ -42,7 +64,9 @@ namespace Basics.Controllers
                 new Claim(ClaimTypes.Name, "Bob"),
                 new Claim(ClaimTypes.Email, "Bob@fmail.com"),
                 new Claim(ClaimTypes.DateOfBirth, "11/11/2000"),
+                new Claim(ClaimTypes.Role, "AdminTwo"),
                 new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(DynamicPolicies.SecurityLevel, "7"),
                 new Claim("Grandma.Says", "Very nice boi.")
             };
 
@@ -62,5 +86,21 @@ namespace Basics.Controllers
             return RedirectToAction("Index");
         }
         
+        public async Task<IActionResult> DoStuff(
+            [FromServices] IAuthorizationService AuthorizationService)
+        {
+            // we are doing stuff here 
+            var builder = new AuthorizationPolicyBuilder("Schema");
+            var customPolicy = builder.RequireClaim("Hello").Build();
+
+            var authResult = await _authorizationService.AuthorizeAsync(User, customPolicy);
+
+            if(authResult.Succeeded)
+            {
+                return View("Index");
+            }
+
+            return View("Index");
+        }
     }
 }
